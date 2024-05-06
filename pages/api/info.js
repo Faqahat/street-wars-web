dce78e6e080d4ffeaa7c7d78d103b5e708cee336
Waleed <faqahatk@gmail.com>
2024-05-06 20:29:51 +0500

    Updated

--- pages/api/info.js
+++ pages/api/info.js
@@ -1,221 +1,220 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-
-
-const dgram = require('dgram')
-
+const dgram = require('dgram');
 
 export default async function handler(req, res) {
-  await query({host:"217.182.46.69"})
-  .then((data) => 
-    res.status(200).json(data)
-  )
-  .catch((e) => {
-    console.log("aa"+e)
-    return res.status(200).json("error")
-  })
-  
-}       
-
+  await query({ host: '185.107.96.148' })
+    .then((data) => res.status(200).json(data))
+    .catch((e) => {
+      console.log('Query Error: ' + e);
+      return res.status(200).json('error');
+    });
+}
 
 function query(options) {
   return new Promise((resolve, reject) => {
-    if (typeof options === 'string') options.host = options
-    options.port = options.port || 7777
-    options.timeout = options.timeout || 10000
+    if (typeof options === 'string') options.host = options;
+    options.port = options.port || 7777;
+    options.timeout = options.timeout || 10000;
 
-    if (!options.host) return reject('Invalid port')
+    if (!options.host) return reject('Invalid Port');
 
     if (!isFinite(options.port) || options.port < 1 || options.port > 65535)
-      return reject('Invalid port')
+      return reject('Invalid Port');
 
-      let response = {}
+    let response = {};
 
     request(options, 'i')
       .then((information) => {
-        response.address = options.host
-        response.port = options.port
-        response.hostname = information.hostname
-        response.gamemode = information.gamemode
-        response.mapname = information.mapname
-        response.passworded = information.passworded === 1
-        response.maxplayers = information.maxplayers
-        response.online = information.players
+        response.address = options.host;
+        response.port = options.port;
+        response.hostname = information.hostname;
+        response.gamemode = information.gamemode;
+        response.mapname = information.mapname;
+        response.passworded = information.passworded === 1;
+        response.maxplayers = information.maxplayers;
+        response.online = information.players;
 
         request(options, 'r').then((rules) => {
-          rules.lagcomp = rules.lagcomp === 'On'
-          rules.weather = parseInt(rules.weather, 10) || null
-          response.rules = rules
+          rules.lagcomp = rules.lagcomp === 'On';
+          rules.weather = parseInt(rules.weather, 10) || null;
+          response.rules = rules;
 
           if (response.online > 100) {
-            response.players = []
-            return resolve(response)
+            response.players = [];
+            return resolve(response);
           } else {
             request(options, 'd')
               .then((players) => {
-                response.players = players
-                return resolve(response)
+                response.players = players;
+                return resolve(response);
               })
               .catch((e) => {
-                return reject(e)
-              })
+                return reject(e);
+              });
           }
-        })
+        });
       })
       .catch((e) => {
-        return reject(e)
-      })
-  })
+        return reject(e);
+      });
+  });
 }
 
 function request(options, opcode) {
   return new Promise((resolve, reject) => {
-    const socket = dgram.createSocket('udp4')
-    const packet = Buffer.alloc(10 + opcode.length)
+    const socket = dgram.createSocket('udp4');
+    const packet = Buffer.alloc(10 + opcode.length);
 
-    packet.write('SAMP')
+    packet.write('SAMP');
 
-    for (let i = 0; i < 4; ++i) packet[i + 4] = options.host.split('.')[i]
+    for (let i = 0; i < 4; ++i) packet[i + 4] = options.host.split('.')[i];
 
-    packet[8] = options.port & 0xff
-    packet[9] = (options.port >> 8) & 0xff
-    packet[10] = opcode.charCodeAt(0)
+    packet[8] = options.port & 0xff;
+    packet[9] = (options.port >> 8) & 0xff;
+    packet[10] = opcode.charCodeAt(0);
 
     try {
-      socket.send(packet, 0, packet.length, options.port, options.host, function (error) {
-        if (error) return reject(error)
-      })
+      socket.send(
+        packet,
+        0,
+        packet.length,
+        options.port,
+        options.host,
+        function (error) {
+          if (error) return reject(error);
+        },
+      );
     } catch (error) {
-      return reject(error)
+      return reject(error);
     }
 
-    let controller = undefined
+    let controller = undefined;
 
     const onTimeOut = function () {
-      socket.close()
-      return reject('Host unavailable')
-    }
+      socket.close();
+      return reject('Host unavailable');
+    };
 
-    controller = setTimeout(onTimeOut, options.timeout)
+    controller = setTimeout(onTimeOut, options.timeout);
 
     socket.on('message', function (message) {
-      if (controller) clearTimeout(controller)
+      if (controller) clearTimeout(controller);
 
-      if (message.length < 11) return resolve(true)
+      if (message.length < 11) return resolve(true);
       else {
-        socket.close()
+        socket.close();
 
-        message = message.slice(11)
+        message = message.slice(11);
 
-        let object = {}
-        let array = []
-        let strlen = 0
-        let offset = 0
+        let object = {};
+        let array = [];
+        let strlen = 0;
+        let offset = 0;
 
         try {
           if (opcode == 'i') {
-            object.passworded = message.readUInt8(offset)
-            offset += 1
+            object.passworded = message.readUInt8(offset);
+            offset += 1;
 
-            object.players = message.readUInt16LE(offset)
-            offset += 2
+            object.players = message.readUInt16LE(offset);
+            offset += 2;
 
-            object.maxplayers = message.readUInt16LE(offset)
-            offset += 2
+            object.maxplayers = message.readUInt16LE(offset);
+            offset += 2;
 
-            strlen = message.readUInt16LE(offset)
-            offset += 4
+            strlen = message.readUInt16LE(offset);
+            offset += 4;
 
-            object.hostname = decode(message.slice(offset, (offset += strlen)))
+            object.hostname = decode(message.slice(offset, (offset += strlen)));
 
-            strlen = message.readUInt16LE(offset)
-            offset += 4
+            strlen = message.readUInt16LE(offset);
+            offset += 4;
 
-            object.gamemode = decode(message.slice(offset, (offset += strlen)))
+            object.gamemode = decode(message.slice(offset, (offset += strlen)));
 
-            strlen = message.readUInt16LE(offset)
-            offset += 4
+            strlen = message.readUInt16LE(offset);
+            offset += 4;
 
-            object.mapname = decode(message.slice(offset, (offset += strlen)))
+            object.mapname = decode(message.slice(offset, (offset += strlen)));
 
-            return resolve(object)
+            return resolve(object);
           }
 
           if (opcode == 'r') {
-            let rulecount = message.readUInt16LE(offset)
-            offset += 2
+            let rulecount = message.readUInt16LE(offset);
+            offset += 2;
 
-            let property = undefined
-            let value = undefined
+            let property = undefined;
+            let value = undefined;
 
             while (rulecount) {
-              strlen = message.readUInt8(offset)
-              ++offset
+              strlen = message.readUInt8(offset);
+              ++offset;
 
-              property = decode(message.slice(offset, (offset += strlen)))
+              property = decode(message.slice(offset, (offset += strlen)));
 
-              strlen = message.readUInt8(offset)
-              ++offset
+              strlen = message.readUInt8(offset);
+              ++offset;
 
-              value = decode(message.slice(offset, (offset += strlen)))
+              value = decode(message.slice(offset, (offset += strlen)));
 
-              object[property] = value
+              object[property] = value;
 
-              --rulecount
+              --rulecount;
             }
 
-            return resolve(object)
+            return resolve(object);
           }
 
           if (opcode == 'd') {
-            let playercount = message.readUInt16LE(offset)
-            offset += 2
+            let playercount = message.readUInt16LE(offset);
+            offset += 2;
 
-            let player = undefined
+            let player = undefined;
 
             while (playercount) {
-              player = {}
+              player = {};
 
-              player.id = message.readUInt8(offset)
-              ++offset
+              player.id = message.readUInt8(offset);
+              ++offset;
 
-              strlen = message.readUInt8(offset)
-              ++offset
+              strlen = message.readUInt8(offset);
+              ++offset;
 
-              player.name = decode(message.slice(offset, (offset += strlen)))
+              player.name = decode(message.slice(offset, (offset += strlen)));
 
-              player.score = message.readUInt16LE(offset)
-              offset += 4
+              player.score = message.readUInt16LE(offset);
+              offset += 4;
 
-              player.ping = message.readUInt16LE(offset)
-              offset += 4
+              player.ping = message.readUInt16LE(offset);
+              offset += 4;
 
-              array.push(player)
+              array.push(player);
 
-              --playercount
+              --playercount;
             }
 
-            return resolve(array)
+            return resolve(array);
           }
         } catch (exception) {
-          return reject(exception)
+          return reject(exception);
         }
       }
-    })
-  })
+    });
+  });
 }
 
 function decode(buffer) {
-  let charset = ''
-  for (let i = 0; i < 128; i++) charset += String.fromCharCode(i)
+  let charset = '';
+  for (let i = 0; i < 128; i++) charset += String.fromCharCode(i);
   charset +=
-    '€�‚ƒ„…†‡�‰�‹�����‘’“”•–—�™�›���� ΅Ά£¤¥¦§¨©�«¬­®―°±²³΄µ¶·ΈΉΊ»Ό½ΎΏΐΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡ�ΣΤΥΦΧΨΩΪΫάέήίΰαβγδεζηθικλμνξοπρςστυφχψωϊϋόύώ�'
-    let charsetBuffer = Buffer.from(charset, 'ucs2')
-    let decodeBuffer = Buffer.alloc(buffer.length * 2)
+    '€�‚ƒ„…†‡�‰�‹�����‘’“”•–—�™�›���� ΅Ά£¤¥¦§¨©�«¬­®―°±²³΄µ¶·ΈΉΊ»Ό½ΎΏΐΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡ�ΣΤΥΦΧΨΩΪΫάέήίΰαβγδεζηθικλμνξοπρςστυφχψωϊϋόύώ�';
+  let charsetBuffer = Buffer.from(charset, 'ucs2');
+  let decodeBuffer = Buffer.alloc(buffer.length * 2);
   for (let i = 0; i < buffer.length; i++) {
-    decodeBuffer[i * 2] = charsetBuffer[buffer[i] * 2]
-    decodeBuffer[i * 2 + 1] = charsetBuffer[buffer[i] * 2 + 1]
+    decodeBuffer[i * 2] = charsetBuffer[buffer[i] * 2];
+    decodeBuffer[i * 2 + 1] = charsetBuffer[buffer[i] * 2 + 1];
   }
-  return decodeBuffer.toString('ucs2')
+  return decodeBuffer.toString('ucs2');
 }
-
